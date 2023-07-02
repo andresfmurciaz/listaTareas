45fcb9f4d7f27b6f726cdfcbc0a76a0ba7c235e3
andresf.murciaz <andresf.murciaz@gmail.com>
2023-07-02 12:07:42 -0500

    input value 2

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 const btnAdd = document.querySelector("[data-btn-add]");
 
 // se cre un evento que escuche cuando el usuario de la click
-btnAdd.addEventListener("click", function(){
+btnAdd.addEventListener("click", (evento) => {
+    evento.preventDefault();
 // capturamos el input y con value mostramos lo que tiene el input
     const input = document.querySelector("[data-form-input]");
     console.log(input.value);
