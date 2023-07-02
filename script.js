d11faedaa253c1d79d0c4a8560ca4f5e220e2b22
andresf.murciaz <andresf.murciaz@gmail.com>
2023-07-02 11:47:22 -0500

    se creo un vento que escuche el click

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // capturo elemento por medio del DOM
 const btnAdd = document.querySelector("[data-btn-add]");
 
-
+// se cre un evento que escuche cuando el usuario de la click
 btnAdd.addEventListener("click", function(){
-console.log('helo');
+
+    console.log('helo');
 
 })
