df7a315fb785acd422bc3e5e4f6fc23a5347cd92
andresf.murciaz <andresf.murciaz@gmail.com>
2023-07-02 12:11:24 -0500

    create task metodo

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // capturo elemento por medio del DOM
 const btnAdd = document.querySelector("[data-btn-add]");
 
-// se cre un evento que escuche cuando el usuario de la click
-btnAdd.addEventListener("click", (evento) => {
-    evento.preventDefault();
-// capturamos el input y con value mostramos lo que tiene el input
-    const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+// se crea un metodo para ser mas organizados
+const createTask =(evento) => 
+    {
+        evento.preventDefault();
+    // capturamos el input y con value mostramos lo que tiene el input
+        const input = document.querySelector("[data-form-input]");
+        console.log(input.value);
+    };
 
-})
+// se cre un evento que escuche cuando el usuario de la click
+btnAdd.addEventListener("click", createTask)
