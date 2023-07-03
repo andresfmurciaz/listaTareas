88df69d3feebd420e0717133229a145482becb74
andresf.murciaz <andresf.murciaz@gmail.com>
2023-07-03 15:51:44 -0500

    appendchild

--- script.js
+++ script.js
@@ -5,30 +5,39 @@ const btnAdd = document.querySelector("[data-btn-add]");
 const createTask =(evento) => 
     {
         evento.preventDefault();
-         // capturamos el input y con value mostramos lo que tiene el input
+        // capturamos el input y con value mostramos lo que tiene el input // se captura el ul el cual es el contenedor de la card
         const input = document.querySelector("[data-form-input]");
-        // se captura el ul el cual es el contenedor de la card
         const list = document.querySelector("[data-list]");
+        const value = input.value;
+        input.value="";
+
         // creamos un elemento li para ahcerlo dinamico y no estatico
         const task= document.createElement("li");
+        const taskContent = document.createElement("div");
+        
         // le agrabamos a ese elemento li su clase card para que coja los css
         task.classList.add("card");
-        console.log(task)
-        const value = input.value;
-         //    nos limpia el value
-        input.value="";
+        
+        taskContent.appendChild(checkComplete());
+        
         const content = `    
-                <div>
-                <i class="far fa-check-square icon"></i>
                 <span class="task">${value}</span>
-                </div>
                 <i class="fas fa-trash-alt trashIcon icon"></i>`
-        console.log(content);
-        // se le agrega al task el codigo content
-        task.innerHTML=content;
+        task.appendChild(taskContent);
         // agregamos a ese elemento list el codigo task como hijo
-        list.appendChild(task)
+        list.appendChild(task);
+
     };
 
 // se cre un evento que escuche cuando el usuario de la click
-btnAdd.addEventListener("click", createTask)
+btnAdd.addEventListener('click', createTask);
+ 
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far");
+    i.classList.add("fa-check-square");
+    i.classList.add("icon");
+
+    return i;
+
+    }
