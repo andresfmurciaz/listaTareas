6f96efe4c67350a1ba7657ebbb372cab50ac2ed2
andresf.murciaz <andresf.murciaz@gmail.com>
2023-07-03 12:42:17 -0500

    value

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ const createTask =(evento) =>
         evento.preventDefault();
     // capturamos el input y con value mostramos lo que tiene el input
         const input = document.querySelector("[data-form-input]");
-        console.log(input.value);
+        const value = input.value;
+    //    nos limpia el value
+       input.value="";
+
+
+
+
     };
 
 // se cre un evento que escuche cuando el usuario de la click
