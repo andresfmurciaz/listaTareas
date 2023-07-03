3753f378fcb1e7f8d4f9f41de9214633e8479661
andresf.murciaz <andresf.murciaz@gmail.com>
2023-07-03 13:04:10 -0500

    template

--- script.js
+++ script.js
@@ -5,17 +5,25 @@ const btnAdd = document.querySelector("[data-btn-add]");
 const createTask =(evento) => 
     {
         evento.preventDefault();
-    // capturamos el input y con value mostramos lo que tiene el input
+         // capturamos el input y con value mostramos lo que tiene el input
         const input = document.querySelector("[data-form-input]");
-// capturamos la card que muestra cada tarea
+         // capturamos la card que muestra cada tarea
         const task= document.querySelector("[data-task]")
         console.log(task)
         const value = input.value;
-    //    nos limpia el value
-       input.value="";
+         //    nos limpia el value
+        input.value="";
 
+        const content = `    
+                <div>
+                <i class="far fa-check-square icon"></i>
+                <span class="task">${value}</span>
+                </div>
+                <i class="fas fa-trash-alt trashIcon icon"></i>`
 
+        console.log(content);
 
+        task.innerHTML=content;
 
     };
 
