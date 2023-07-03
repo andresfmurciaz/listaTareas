fd4d53f27e21490d169b96d71fe652f879886527
andresf.murciaz <andresf.murciaz@gmail.com>
2023-07-03 13:23:03 -0500

    comentarios codigo

--- script.js
+++ script.js
@@ -7,28 +7,26 @@ const createTask =(evento) =>
         evento.preventDefault();
          // capturamos el input y con value mostramos lo que tiene el input
         const input = document.querySelector("[data-form-input]");
-
+        // se captura el ul el cual es el contenedor de la card
         const list = document.querySelector("[data-list]");
-         
-
-
+        // creamos un elemento li para ahcerlo dinamico y no estatico
         const task= document.createElement("li");
+        // le agrabamos a ese elemento li su clase card para que coja los css
+        task.classList.add("card");
         console.log(task)
         const value = input.value;
          //    nos limpia el value
         input.value="";
-
         const content = `    
                 <div>
                 <i class="far fa-check-square icon"></i>
                 <span class="task">${value}</span>
                 </div>
                 <i class="fas fa-trash-alt trashIcon icon"></i>`
-
         console.log(content);
-
+        // se le agrega al task el codigo content
         task.innerHTML=content;
-
+        // agregamos a ese elemento list el codigo task como hijo
         list.appendChild(task)
     };
 
